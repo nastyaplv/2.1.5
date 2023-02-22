ae66110ac7464d6ea2751d2ea0c431b68e703c7e
nastyaplv <nastyaplv@mail.ru>
2023-02-23 00:49:20 +0300

    2.1.5 сделана

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    private Duck5 dark;
+
+    @Autowired
+    public void setDuck5(Duck5 dark) {
+        this.dark = dark;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + dark.toString();
     }
 }
